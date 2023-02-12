dc306f354dfb3e5359def7bbcd278a0c862f452a
Qdesnitsa <lenkasid@tut.by>
2023-02-12 14:21:01 +0300

    finish UI tests basics

--- Automation.Extensions/Components/WebDriverExtensions.cs
+++ Automation.Extensions/Components/WebDriverExtensions.cs
@@ -1,10 +1,13 @@
 ﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Support.Extensions;
 using OpenQA.Selenium.Support.UI;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
+using System.Threading;
 
 namespace Automation.Extensions.Components
 {
@@ -56,6 +59,72 @@ namespace Automation.Extensions.Components
                 var elements = d.FindElements(by).Where(i => i.Displayed).ToList();
                 return new ReadOnlyCollection<IWebElement>(elements);
             });
+        }
+
+        public static IWebElement GetEnabledElement(this IWebDriver driver, By by) => GetEnabledElement(driver, by, TimeSpan.FromSeconds(15));
+
+        public static IWebElement GetEnabledElement(this IWebDriver driver, By by, TimeSpan timeout)
+        {
+            var wait = new WebDriverWait(driver, timeout);
+            return wait.Until(d =>
+            {
+                var element = d.FindElement(by);
+                return element.Enabled ? element : null;
+            });
+        }
+
+        public static IWebDriver VerticalWindowScroll(this IWebDriver driver, int scrollAmount)
+        {
+            driver.ExecuteJavaScript($"window.scroll(0,{scrollAmount});");
+            return driver;
+        }
+
+        public static Actions Actions(this IWebElement element)
+        {
+            var driver = ((IWrapsDriver) element).WrappedDriver;
+            var actions = new Actions(driver);
+            return actions.MoveToElement(element);
+        }
+
+        public static IWebElement ForceClick(this IWebElement element)
+        {
+            var driver = ((IWrapsDriver)element).WrappedDriver;
+            driver.ExecuteJavaScript("arguments[0].click();", element);
+            return element;
+        }
+
+        public static IWebElement SendKeys(this IWebElement element, string text, int interval)
+        {
+            foreach (var @char in text)
+            {
+                element.SendKeys($"{@char}");
+                Thread.Sleep(interval);
+            }
+            return element;
+        }
+
+        public static IWebElement ForceClear(this IWebElement element)
+        {
+            var value = element.GetAttribute("value");
+            element.SendKeys(Keys.End);
+            for (int i = 0; i < value.Length; i++)
+            {
+                element.SendKeys(Keys.Backspace);
+                Thread.Sleep(50);
+            }
+            return element;
+        }
+
+        public static IWebDriver SubmitForm(this IWebDriver driver, int index)
+        {
+            driver.ExecuteJavaScript($"document.forms[{index}].submit();");
+            return driver;
+        }
+
+        public static IWebDriver SubmitForm(this IWebDriver driver, string id)
+        {
+            driver.ExecuteJavaScript($"document.forms[\"{id}\"].submit();");
+            return driver;
         }
     }
 }
--- Automation.Testing/SeleniumSample.cs
+++ Automation.Testing/SeleniumSample.cs
@@ -8,6 +8,7 @@ using OpenQA.Selenium.IE;
 using OpenQA.Selenium.Support.UI;
 using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -124,6 +125,82 @@ namespace Automation.Testing
             var driver = new WebDriverFactory(new DriverParams { Driver = "chrome", Binaries = @"C:\Users\User\CSharpProjects\AutomationRoot\Automation.Testing\Resources" }).Get();
             driver.GoToUrl("https://gravitymvctestapplication.azurewebsites.net/");
             var elements = driver.GetVisibleElements(By.XPath("//li[@class='nav-item']"));
+            Thread.Sleep(2000);
+            driver.Dispose();
+        }
+
+        [TestMethod]
+        public void GetEnabledElementSample()
+        {
+            var driver = new WebDriverFactory(new DriverParams { Driver = "chrome", Binaries = @"C:\Users\User\CSharpProjects\AutomationRoot\Automation.Testing\Resources" }).Get();
+            driver.GoToUrl("https://gravitymvctestapplication.azurewebsites.net/Student");
+            // to make element disabled write on HTML page disabled - test will fail
+            driver.GetEnabledElement(By.XPath("//input[@name='SearchString']")).SendKeys("hello");
+            Thread.Sleep(2000);
+            driver.Dispose();
+        }
+
+        [TestMethod]
+        public void VerticalWindowScrollSample()
+        {
+            var driver = new WebDriverFactory(new DriverParams { Driver = "chrome", Binaries = @"C:\Users\User\CSharpProjects\AutomationRoot\Automation.Testing\Resources" }).Get();
+            driver.GoToUrl("https://gravitymvctestapplication.azurewebsites.net/Student");
+            driver.Manage().Window.Size = new Size(100, 350);
+            driver.VerticalWindowScroll(1000);
+            Thread.Sleep(2000);
+            driver.Dispose();
+        }
+
+        [TestMethod]
+        public void ActionsSample()
+        {
+            var driver = new WebDriverFactory(new DriverParams { Driver = "chrome", Binaries = @"C:\Users\User\CSharpProjects\AutomationRoot\Automation.Testing\Resources" }).Get();
+            driver.GoToUrl("https://gravitymvctestapplication.azurewebsites.net/");
+            driver.GetVisibleElement(By.XPath("//a[.='Students']")).Actions().Click().Build().Perform();
+            Thread.Sleep(2000);
+            driver.Dispose();
+        }
+
+        [TestMethod]
+        public void ForceClickSample()
+        {
+            var driver = new WebDriverFactory(new DriverParams { Driver = "chrome", Binaries = @"C:\Users\User\CSharpProjects\AutomationRoot\Automation.Testing\Resources" }).Get();
+            driver.GoToUrl("https://gravitymvctestapplication.azurewebsites.net/");
+            driver.GetElement(By.XPath("//a[.='Students']")).ForceClick();
+            Thread.Sleep(2000);
+            driver.Dispose();
+        }
+
+        [TestMethod]
+        public void SendKeysIntervalSample()
+        {
+            var driver = new WebDriverFactory(new DriverParams { Driver = "chrome", Binaries = @"C:\Users\User\CSharpProjects\AutomationRoot\Automation.Testing\Resources" }).Get();
+            driver.GoToUrl("https://gravitymvctestapplication.azurewebsites.net/Student");
+            driver.GetEnabledElement(By.XPath("//input[@name='SearchString']")).SendKeys("hello", 1000);
+            Thread.Sleep(2000);
+            driver.Dispose();
+        }
+
+        [TestMethod]
+        public void ForceClearSample()
+        {
+            var driver = new WebDriverFactory(new DriverParams { Driver = "chrome", Binaries = @"C:\Users\User\CSharpProjects\AutomationRoot\Automation.Testing\Resources" }).Get();
+            driver.GoToUrl("https://gravitymvctestapplication.azurewebsites.net/Student");
+            var element = driver.GetEnabledElement(By.XPath("//input[@name='SearchString']"));
+            element.SendKeys("hello", 100);
+            element.SendKeys(Keys.Home);
+            element.ForceClear();
+            Thread.Sleep(2000);
+            driver.Dispose();
+        }
+
+        [TestMethod]
+        public void SubmitFormSample()
+        {
+            var driver = new WebDriverFactory(new DriverParams { Driver = "chrome", Binaries = @"C:\Users\User\CSharpProjects\AutomationRoot\Automation.Testing\Resources" }).Get();
+            driver.GoToUrl("https://gravitymvctestapplication.azurewebsites.net/Student");
+            driver.GetEnabledElement(By.XPath("//input[@name='SearchString']")).SendKeys("Alexander");
+            driver.SubmitForm(0);
             Thread.Sleep(2000);
             driver.Dispose();
         }
